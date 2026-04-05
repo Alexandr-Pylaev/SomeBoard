5f18585323038ab1395b228aacf64a76f585a065
Alexandr-Pylaev <236025710+Alexandr-Pylaev@users.noreply.github.com>
2026-04-05 20:22:58 +0300

    Add Posting controller

---
+++ SomeBoard.Backend/Controllers/PostingController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SomeBoard.Backend.Context;
+using SomeBoard.Backend.Models;
+using SomeBoard.Shared;
+using SomeBoard.Shared.Posting;
+
+namespace SomeBoard.Backend.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class PostingController : ControllerBase
+{
+    [HttpGet]
+    [ActionName("Post")]
+    public ServerPostDTO[] Fetch(int position, PostingContext context)
+    {
+        return context.Fetch(position, 25).Select(x => IDTODeserializable<ServerPostDTO>
+            .Convert<PostModel>(x) 
+            .ToDTO()).ToArray();
+    }
+    
+    [HttpPost]
+    [ActionName("Post")]
+    public ServerPostDTO Publish(CreatePostDTO input, PostingContext context)
+    {
+        return IDTODeserializable<ServerPostDTO>
+            .Convert<PostModel>(context.Publish(input.Author, input.Message, DateTime.Now)) 
+            .ToDTO();
+    }
+}
