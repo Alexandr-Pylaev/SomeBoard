c060a2378ee37fbbdb2a79c6a41934a8bec89102
Alexandr-Pylaev <236025710+Alexandr-Pylaev@users.noreply.github.com>
2026-04-05 20:09:58 +0300

    Fix multiple IDTOSerializable, IDTODeserializable not working

--- SomeBoard.Shared/IDTODeserializable.cs
+++ SomeBoard.Shared/IDTODeserializable.cs
@@ -3,4 +3,5 @@
 public interface IDTODeserializable<TDTO>
 {
     public TDTO ToDTO();
+    public static IDTODeserializable<TDTO> Convert<T>(T val) where T : IDTODeserializable<TDTO> => (IDTODeserializable<TDTO>) val!;
 }
--- SomeBoard.Shared/IDTOSerializable.cs
+++ SomeBoard.Shared/IDTOSerializable.cs
@@ -3,4 +3,5 @@
 public interface IDTOSerializable <TSelf, TDTO>
 {
     public TSelf FromDTO(TDTO dto);
+    public static IDTOSerializable<TSelf, TDTO> Convert<T>(TSelf val) where T : IDTOSerializable<TSelf, TDTO> => (IDTOSerializable<TSelf, TDTO>) val!;
 }
