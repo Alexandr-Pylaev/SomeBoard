b33f17342a058d698f2d866a5a060f953161d3dd
Alexandr-Pylaev <236025710+Alexandr-Pylaev@users.noreply.github.com>
2026-04-05 17:39:49 +0300

    Rename project to SomeBoard

--- SomeBoard.Frontend/Pages/Privacy.cshtml.cs
+++ SomeBoard.Frontend/Pages/Privacy.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Frontend.Pages;
+namespace SomeBoard.Frontend.Pages;
 
 public class PrivacyModel : PageModel
 {
