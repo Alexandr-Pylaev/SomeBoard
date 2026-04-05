cb48ed5bf8010dd98a6e29a67ca118e5d3ec4083
Alexandr-Pylaev <236025710+Alexandr-Pylaev@users.noreply.github.com>
2026-04-05 20:34:15 +0300

    Fix nullability for IDTO converters

--- SomeBoard.Shared/IDTOSerializable.cs
+++ SomeBoard.Shared/IDTOSerializable.cs
@@ -3,5 +3,5 @@
 public interface IDTOSerializable <TSelf, TDTO>
 {
     public TSelf FromDTO(TDTO dto);
-    public static IDTOSerializable<TSelf, TDTO> Convert<T>(TSelf val) where T : IDTOSerializable<TSelf, TDTO> => (IDTOSerializable<TSelf, TDTO>) val!;
+    public static IDTOSerializable<TSelf, TDTO>? Convert<T>(TSelf? val) where T : IDTOSerializable<TSelf, TDTO> => (IDTOSerializable<TSelf, TDTO>?) val;
 }
