285ecb5540307dcf440002d4bbf022052957565d
Alexandr-Pylaev <236025710+Alexandr-Pylaev@users.noreply.github.com>
2026-04-05 17:46:55 +0300

    Create Post class

---
+++ SomeBoard.Shared/Posting/Post.cs
@@ -0,0 +1,11 @@
+﻿namespace SomeBoard.Shared.Posting;
+
+/// <summary>
+/// Main information about post on board
+/// </summary>
+public class Post
+{
+    public string Author;
+    public string Message;
+    public DateTime PublishTime;
+}
