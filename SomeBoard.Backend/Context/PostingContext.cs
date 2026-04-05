e5e2966a387a53118c50b14755f8d4675a71638d
Alexandr-Pylaev <236025710+Alexandr-Pylaev@users.noreply.github.com>
2026-04-05 18:01:53 +0300

    Create PostingContext class

---
+++ SomeBoard.Backend/Context/PostingContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SomeBoard.Backend.Models;
+
+namespace SomeBoard.Backend.Context;
+
+public class PostingContext : DbContext
+{
+    public DbSet<PostModel> Posts { get; set; }
+
+    public PostingContext() { }
+}
