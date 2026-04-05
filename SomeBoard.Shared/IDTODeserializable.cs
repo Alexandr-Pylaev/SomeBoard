5826e668eda5351332886c811f4f909e3fa6fc65
Alexandr-Pylaev <236025710+Alexandr-Pylaev@users.noreply.github.com>
2026-04-05 20:39:12 +0300

    Fix cb48ed5

--- SomeBoard.Shared/IDTODeserializable.cs
+++ SomeBoard.Shared/IDTODeserializable.cs
@@ -3,5 +3,5 @@
 public interface IDTODeserializable<TDTO>
 {
     public TDTO ToDTO();
-    public static IDTODeserializable<TDTO>? Convert<T>(T? val) where T : IDTODeserializable<TDTO> => val;
+    public static IDTODeserializable<TDTO>? Convert<T>(T? val) where T : IDTODeserializable<TDTO>? => val;
 }
