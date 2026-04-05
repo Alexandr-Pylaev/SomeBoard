daae90665bf359b4e9186854d812e0f28bd26d26
Alexandr-Pylaev <236025710+Alexandr-Pylaev@users.noreply.github.com>
2026-04-05 17:54:16 +0300

    Add constructor and default to post class

--- SomeBoard.Shared/Posting/Post.cs
+++ SomeBoard.Shared/Posting/Post.cs
@@ -5,7 +5,16 @@
 /// </summary>
 public class Post
 {
-    public string Author;
-    public string Message;
-    public DateTime PublishTime;
+    public Post(string author, string message, DateTime publishTime)
+    {
+        Author = author;
+        Message = message;
+        PublishTime = publishTime;
+    }
+
+    public string Author { get; set; }
+    public string Message { get; set; }
+    public DateTime PublishTime { get; set; }
+
+    public static readonly Post Empty = new Post(String.Empty,String.Empty, DateTime.MinValue);
 }
