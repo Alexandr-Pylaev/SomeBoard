e4387abce00a758f65775f55a7824ab5182c94da
Alexandr-Pylaev <236025710+Alexandr-Pylaev@users.noreply.github.com>
2026-04-06 17:24:34 +0300

    Add fetching posts for frontend

--- SomeBoard.Frontend/Assets.cs
+++ SomeBoard.Frontend/Assets.cs
@@ -5,15 +5,18 @@ public class Assets
     public const string CONFIG_PATH = "SomeBoard";
     public const string BOARD_DESC_PATH = $"{CONFIG_PATH}:Board:Description";
     public const string BOARD_NAME_PATH = $"{CONFIG_PATH}:Board:Name";
+    public const string BOARD_BACKEND_URL_PATH = $"{CONFIG_PATH}:Board:Backend:URL";
     public static Assets Singleton => _singleton.Value;
     private static readonly Lazy<Assets> _singleton = new();
 
     public string BoardDescription = "Public text board";
-    public string BoardName = "SomeBoard"; 
+    public string BoardName = "SomeBoard";
+    public string? BoardBackendUrl;
 
     public void Initialize(IConfiguration configuration)
     {
         BoardDescription = configuration.GetValue<string>(BOARD_DESC_PATH, BoardDescription);
         BoardName = configuration.GetValue<string>(BOARD_NAME_PATH, BoardName);
+        BoardBackendUrl = configuration.GetValue<string?>(BOARD_BACKEND_URL_PATH, null);
     }
 }
--- SomeBoard.Frontend/Pages/Index.cshtml.cs
+++ SomeBoard.Frontend/Pages/Index.cshtml.cs
@@ -1,5 +1,8 @@
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RestSharp;
+using SomeBoard.Shared.Posting;
 
 namespace SomeBoard.Frontend.Pages;
 
@@ -14,5 +17,43 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
+        RestClient client = new RestClient(
+            new RestClientOptions(Assets.Singleton.BoardBackendUrl 
+                                  ?? throw new NullReferenceException("Cannot access backend: Backend URL is null.")));
+
+        var result = client.Get(new RestRequest("/post"));
+        try
+        {
+            var posts = JsonSerializer.Deserialize<ServerPostDTO[]>(result.Content ?? "[]") ?? [];
+            ViewData.Add("Posts", posts);
+        }
+        catch (JsonException _)
+        {
+            try
+            {
+                var error = JsonSerializer.Deserialize<ErrorDTO>(result.Content ?? "{}");
+                if (error is null) throw;
+                AddError($"Error: {error.ErrorText}\nError code: {error.ErrorCode}");
+            }
+            catch (JsonException ex2)
+            {
+                AddError("Received invalid respond from server.");
+                Console.WriteLine(ex2);
+            }
+        }
+    }
+
+    private void AddError(string error)
+    {
+        if (ViewData.TryGetValue("Errors", out var ls))
+        {
+            var errors = (List<String>?)ls;
+            errors?.Add(error);
+        }
+        else
+        {
+            if (!ViewData.TryAdd("Errors", new List<string> { error }))
+                throw new InvalidOperationException("Errors list cannot be added or updated.");
+        }
     }
 }
